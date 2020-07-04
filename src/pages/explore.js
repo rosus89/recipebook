2bbcea1fab980599f5bff007d5717f15eb305125
Stefan Rosu <rosus89@gmail.com>
2020-07-04 22:14:20 +0100

    Display Tile List

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -1,14 +1,42 @@
 import * as React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { SearchBar } from 'react-native-elements';
+import { LargeTile } from 'components/elements';
+import {List} from 'components/containers';
 
 export default function Explore({ navigation }) {
     const [search, setSearch] = React.useState('');
-
+    
     const updateSearch = (search) => {
         setSearch( search );
     };
+    let mockdata = [
+        {
+            image: "https://hips.hearstapps.com/del.h-cdn.co/assets/17/15/1492181920-delish-sticky-orange-chicken-2.jpg",
+            title: "Cuisine"
+        },
+        {
+            image: "https://hips.hearstapps.com/del.h-cdn.co/assets/17/15/1492181920-delish-sticky-orange-chicken-2.jpg",
+            title: "Occasion"
+        },
+        {
+            image: "https://hips.hearstapps.com/del.h-cdn.co/assets/17/15/1492181920-delish-sticky-orange-chicken-2.jpg",
+            title: "Ingredients"
+        },
+        {
+            image: "https://hips.hearstapps.com/del.h-cdn.co/assets/17/15/1492181920-delish-sticky-orange-chicken-2.jpg",
+            title: "Flavour"
+        },
+        {
+            image: "https://hips.hearstapps.com/del.h-cdn.co/assets/17/15/1492181920-delish-sticky-orange-chicken-2.jpg",
+            title: "Fitness"
+        },
+        {
+            image: "https://hips.hearstapps.com/del.h-cdn.co/assets/17/15/1492181920-delish-sticky-orange-chicken-2.jpg",
+            title: "Most Popular"
+        }
 
+    ]
     return (
         <View>
             <SearchBar
@@ -16,6 +44,12 @@ export default function Explore({ navigation }) {
                 onChangeText={updateSearch}
                 value={search}
             />
+            <List
+                data={mockdata}
+            >
+                <LargeTile image title/>
+            </List>
+
         </View>
     );
 }
