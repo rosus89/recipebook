0d9d131537e5ac3c3800a4a3b67b9b0573743872
Stefan Rosu <rosus89@gmail.com>
2020-07-07 19:48:22 +0100

    Component Header Button

---
+++ src/components/elements/headerButton.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { ImageBackground, Text, TouchableOpacity } from 'react-native';
+import { Icon } from 'react-native-elements'
+
+
+
+function HeaderButton({ onPress }) {
+    return (
+        <TouchableOpacity
+            onPress={onPress}
+        >
+            <Icon
+                name='menu'
+                color='#517fa4'
+                size= {35}
+                iconStyle={{ marginRight: 5}}
+            />
+        </TouchableOpacity>
+    )
+}
+export default HeaderButton;
