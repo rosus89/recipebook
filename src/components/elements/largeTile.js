6a2656952a4e97705c747f02459aef5cb549c49d
Stefan Rosu <rosus89@gmail.com>
2020-07-07 19:44:21 +0100

    Feature: reasponsive

--- src/components/elements/largeTile.js
+++ src/components/elements/largeTile.js
@@ -1,28 +1,49 @@
 import * as React from 'react';
-import {ImageBackground, Text} from 'react-native';
-function LargeTile({key, image, title}){
+import { ImageBackground, Text, TouchableOpacity, StyleSheet} from 'react-native';
+
+
+
+
+export default function LargeTile({image, title, onPress}){
     return(
-        <ImageBackground key={key}
-            source={image}
-            style={{
-                boxSizing: 'border-box',
-                height: 80,
-                width: '100vw',
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center"
-            }}
->
-            <Text
-                style={{
-                    fontSize: 24,
-                    fontWeight: 'bold',
-                    color: 'white',
-                }}
+        <TouchableOpacity
+            key={title}
+            onPress={onPress}
+            style={styles.wrapper}
+        >
+            <ImageBackground
+                source={image}
+                style={styles.tileImage}
             >
-                {title}
-            </Text>
-        </ImageBackground>
+                <Text
+                    style={styles.tileTitle}
+                >
+                    {title}
+                </Text>
+            </ImageBackground>
+        </TouchableOpacity>
     )
 }
-export default LargeTile;
+
+const styles = StyleSheet.create({
+    wrapper: {
+        marginTop: 6,
+        flex: 1
+    },
+
+    tileTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: { width: -2, height: 1 },
+        textShadowRadius: 10
+    },
+
+    tileImage: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1
+    }
+});
