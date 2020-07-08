b5b2626e3add982be017050e15b6a101f5a8441a
Stefan Rosu <rosus89@gmail.com>
2020-07-08 10:24:03 +0100

    route changes page title

--- src/pages/listRecipes.js
+++ src/pages/listRecipes.js
@@ -3,6 +3,8 @@ import { Text, View  } from 'react-native';
 
 export default function ListRecipes({ navigation, route }) {
     console.log(route)
+    navigation.setOptions({ title: route.params })
+
     return (
         <View >
             <Text> {route.params} </Text>
